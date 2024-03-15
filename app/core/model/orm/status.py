29bfd04a167f9bacd1569b571b46cdc417c7456e
Guilherme-Endrigo <endrigo.guilherme@hotmail.com>
2024-03-15 19:12:33 -0300

    aumento campo

--- app/core/model/orm/status.py
+++ app/core/model/orm/status.py
@@ -9,4 +9,4 @@ class Status(Base):
     __tablename__ = "status"
 
     id = Column(INTEGER, primary_key=True, index=True, autoincrement=True)
-    nome = Column(VARCHAR(11), unique=True, index=True, nullable=False)
+    nome = Column(VARCHAR(25), unique=True, index=True, nullable=False)
