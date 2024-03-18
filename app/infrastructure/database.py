141bfa19f52ff6c9b398da61940932cb5c64dc65
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-17 21:15:09 -0300

    deploy-app

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -9,7 +9,7 @@ from sqlalchemy_utils import create_database, database_exists
 
 load_dotenv()
 
-# Pegando as configuracoes do .env
+# Pegando as configuracoes do .env 
 DB_USER = config("POSTGRES_USER", default=os.getenv("POSTGRES_USER"))
 DB_PASSWORD = config("POSTGRES_PASSWORD", default=os.getenv("POSTGRES_PASSWORD"))
 DB_NAME = config("POSTGRES_DB", default=os.getenv("POSTGRES_DB"))
