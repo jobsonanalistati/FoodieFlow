97f06daa4961b6a26d808d870a8cecddcde8bfe7
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-18 20:50:28 -0300

    ajuste-black

--- app/application/commons/secret_manager.py
+++ app/application/commons/secret_manager.py
@@ -7,22 +7,28 @@ from botocore.exceptions import ClientError
 def aws_connection():
     session = boto3.session.Session()
     client = session.client(
-        service_name='secretsmanager',
-        region_name="us-east-1"  # Substitua pela sua região da AWS
+        service_name="secretsmanager",
+        region_name="us-east-1",  # Substitua pela sua região da AWS
     )
     return client
 
 
 def return_variables(client):
     try:
-        get_secret_value_response = client.get_secret_value(SecretId='foodieFlow_secrets')
+        get_secret_value_response = client.get_secret_value(
+            SecretId="foodieFlow_secrets"
+        )
     except ClientError as e:
-        raise Exception("Não foi possível recuperar o secret: {}".format(e.response['Error']['Message']))
+        raise Exception(
+            "Não foi possível recuperar o secret: {}".format(
+                e.response["Error"]["Message"]
+            )
+        )
     else:
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
+        if "SecretString" in get_secret_value_response:
+            secret = get_secret_value_response["SecretString"]
         else:
-            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            secret = base64.b64decode(get_secret_value_response["SecretBinary"])
 
     secret_dict = json.loads(secret)
     return secret_dict
