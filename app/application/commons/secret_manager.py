3323efd984202207747a52cf390af6ee062623f8
Guilherme-Endrigo <endrigo.guilherme@hotmail.com>
2024-03-18 20:46:19 -0300

    implementação secret

---
+++ app/application/commons/secret_manager.py
@@ -0,0 +1,28 @@
+import base64
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+
+def aws_connection():
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name="us-east-1"  # Substitua pela sua região da AWS
+    )
+    return client
+
+
+def return_variables(client):
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId='foodieFlow_secrets')
+    except ClientError as e:
+        raise Exception("Não foi possível recuperar o secret: {}".format(e.response['Error']['Message']))
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+
+    secret_dict = json.loads(secret)
+    return secret_dict
